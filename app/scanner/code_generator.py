fb109737453d3a9ed5b34dcb4341b1cb01a25743
Alejandro Vélez <141779507+MelonConYogurt@users.noreply.github.com>
2024-08-06 15:59:40 -0500

    update data

--- app/scanner/code_generator.py
+++ app/scanner/code_generator.py
@@ -1,14 +1,31 @@
+import os
+from faker import Faker
 from barcode import EAN13
 from barcode.writer import SVGWriter
 
+faker = Faker()
+
+
 def generate_barcode_ean13(code: int):
     try:
-        with open (f"code-{code}.svg", "wb") as f:
+        directory = r"app/scanner/svg"
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        
+        path = os.path.join(directory, f"code-{code}.svg")
+        with open (path, "wb") as f:
             EAN13(str(code), writer= SVGWriter()).write(f)
+            return path
     except Exception as e:
         print(e)
         
-        
-        
+
+def generate_fake_barcode(quantity:int):
+    for _ in range(quantity):
+        new_barcode = generate_barcode_ean13(faker.ean13())
+        print(new_barcode)
+            
+
 if __name__== "__main__":
-    generate_barcode_ean13(1222780790790)
+    generate_fake_barcode(20)
+    pass
