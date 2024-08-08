2a62a46664f66a8c6b5c766355889541f7506beb
Alejandro Vélez <141779507+MelonConYogurt@users.noreply.github.com>
2024-08-08 14:50:12 -0500

    update data

--- .venv/app/sql/pg_data_base.py
+++ .venv/app/sql/pg_data_base.py
@@ -7,12 +7,6 @@ from pathlib import Path
 from faker import Faker
 import mysql.connector
 
-# OpenTelemetry imports
-from opentelemetry import trace
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter
-from mysql.connector.opentelemetry.instrumentation import MySQLInstrumentor as OracleMySQLInstrumentor
-
 #import de scaner module
 from app.scanner.scan_barcode import *
 from app.scanner.code_generator import *
@@ -20,11 +14,13 @@ from app.scanner.code_generator import *
 #pg database
 import psycopg2
 
+#Manage exceptions
+from psycopg2 import sql 
+from psycopg2 import errors
 
 # Load environment variables
 dotenv_path = Path('app/.env')
 load_dotenv(dotenv_path=dotenv_path)
-
 
 
 # Faker instance
@@ -39,31 +35,28 @@ config = {
     "database": os.getenv("DATABASE"),
 }
 
-class data_base():
+class data_base:
     def __init__(self):
         self.connect = None
         self.cursor = None
         try:
-            self.connect = mysql.connector.connect(**config)
-            if self.connect.is_connected():
-                self.cursor = self.connect.cursor()
-                print("Connected to the database")
-            else:
-                print("Failed to connect to the database")
-                raise Exception("Database connection failed")
-        except Exception as e:
-            print(f"Error connecting to the database: {e}")
-            raise
-
+            self.connect = psycopg2.connect(**config)
+            self.cursor = self.connect.cursor()
+            print("Connected to the database")
+        except psycopg2.DatabaseError as err:
+            print("An exception has occurred: ", err)
+            print("Exception TYPE:", type(err))
+    
     def close(self):
-        if self.connect.is_connected():
+        if self.cursor:
             self.cursor.close()
+        if self.connect:
             self.connect.close()
-            print("Database connection closed")
+        print("Database connection closed")
 
     def search_products(self, code: int):
         try:
-            query = ("SELECT * FROM products WHERE product_code = %s")
+            query = "SELECT * FROM products WHERE product_code = %s"
             values = (code,)
             self.cursor.execute(query, values)
             product = self.cursor.fetchone()
@@ -73,94 +66,78 @@ class data_base():
             else:
                 print("Product not found")
                 return False, None
-        except mysql.connector.Error as err:
-            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-                print("Something is wrong with your user name or password")
-            elif err.errno == errorcode.ER_BAD_DB_ERROR:
-                print("Database does not exist")
-            else:
-                print(err)
+        except psycopg2.Error as err:
+            print(f"Error in search_products: {err}")
+            print("Exception TYPE:", type(err))
 
     def insert_products(self, name: str, price: float, code: int, quantity: int, category: Optional[str], description: Optional[str]):
         try:
-            verify_produt = self.search_products(code)
-            if verify_produt[0] == True:
+            verify_product = self.search_products(code)
+            if verify_product[0]:
                 print("Product already exists")
                 return
             else:
                 query = ("INSERT INTO products (product_name, product_price, product_code, product_quantity, product_category, product_description) "
-                            "VALUES (%s, %s, %s, %s, %s, %s)")
+                         "VALUES (%s, %s, %s, %s, %s, %s)")
                 values = (name, price, code, quantity, category, description)
                 self.cursor.execute(query, values)
                 self.connect.commit()
                 print(f"Product {name} inserted successfully")
-        except mysql.connector.Error as err:
-            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-                print("Something is wrong with your user name or password")
-            elif err.errno == errorcode.ER_BAD_DB_ERROR:
-                print("Database does not exist")
-            else:
-                print(err)
+        except psycopg2.Error as err:
+            print(f"Error in insert_products: {err}")
+            print("Exception TYPE:", type(err))
 
-    def delete_products(self, code: int, quantity:int):
+    def delete_products(self, code: int, quantity: int):
         try:
-            #Veriyfy the if product exist
-            verify_produt = self.search_products(code)
-            if verify_produt[0] == False:
+            # Verify if product exists
+            verify_product = self.search_products(code)
+            if not verify_product[0]:
                 print("Product not found")
                 return
             else:
-                #veruify if product quantity is suficent
-                query_verificate_quantity =("SELECT * FROM products WHERE product_code = %s")
-                values= (code,)
+                # Verify if product quantity is sufficient
+                query_verificate_quantity = "SELECT product_quantity FROM products WHERE product_code = %s"
+                values = (code,)
                 self.cursor.execute(query_verificate_quantity, values)
                 current_quantity = self.cursor.fetchone()
 
-                if quantity > current_quantity[4]:
-                    print(f"Insufficient quantity in stock\nThe stok for the product is: {current_quantity[4]}")
+                if current_quantity and quantity > current_quantity[0]:
+                    print(f"Insufficient quantity in stock\nThe stock for the product is: {current_quantity[0]}")
                     return False
                 else:
-                    #Update the product stock
-                    query =( "UPDATE products SET product_quantity = product_quantity - %s WHERE product_code = %s")
+                    # Update the product stock
+                    query = "UPDATE products SET product_quantity = product_quantity - %s WHERE product_code = %s"
                     values = (quantity, code)
                     self.cursor.execute(query, values)
                     self.connect.commit()
                     return True
-        except mysql.connector.Error as err:
-            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-                print("Something is wrong with your user name or password")
-            elif err.errno == errorcode.ER_BAD_DB_ERROR:
-                print("Database does not exist")
-            else:
-                print(err)
-
+        except psycopg2.Error as err:
+            print(f"Error in delete_products: {err}")
+            print("Exception TYPE:", type(err))
+            
     def drop_product(self, code: int):
         try:
-            #Veriyfy the if product exist
-            verify_produt = self.search_products(code)
-            if verify_produt[0] == False:
+            # Verify if product exists
+            verify_product = self.search_products(code)
+            if not verify_product[0]:
                 print("Product not found")
                 return
             else:
-                query=("DELETE FROM products WHERE product_code = %s")
-                values= (code,)
+                query = "DELETE FROM products WHERE product_code = %s"
+                values = (code,)
                 self.cursor.execute(query, values)
                 self.connect.commit()
-                print(f"Procut eliminated:\n {verify_produt[1]}")
-        except mysql.connector.Error as err:
-            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-                print("Something is wrong with your user name or password")
-            elif err.errno == errorcode.ER_BAD_DB_ERROR:
-                print("Database does not exist")
-            else:
-                print(err)
-
+                print(f"Product eliminated:\n {verify_product[1]}")
+        except psycopg2.Error as err:
+            print(f"Error in drop_product: {err}")
+            print("Exception TYPE:", type(err))
+    
     def sale(self):
         try:
             date = datetime.datetime.now().strftime('%Y%m%d')
-            number = faker.random_number(5)
+            number = faker.Faker().random_number(digits=5)
             generate_code = f"{date}{number}"
-            generate_qr(data = generate_code)
+            # generate_qr(data = generate_code)  # Define esta función según tus necesidades
             
             sale_data = {
                 "sale_code": generate_code,
@@ -172,23 +149,19 @@ class data_base():
             values = (sale_data["sale_code"], sale_data["sale_date"], sale_data["sale_total"])  
            
             self.cursor.execute(query, values)
-            new_sale_id = self.cursor.lastrowid
+            new_sale_id = self.cursor.fetchone()[0]  # Obtén el nuevo ID de venta
             self.connect.commit()
             return new_sale_id
-        
-        except mysql.connector.Error as err:
-                if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-                    print("Something is wrong with your user name or password")
-                elif err.errno == errorcode.ER_BAD_DB_ERROR:
-                    print("Database does not exist")
-                else:
-                    print(err)
+        except psycopg2.Error as err:
+            print(f"Error in sale: {err}")
+            print("Exception TYPE:", type(err))
+            
                          
     def sale_products(self):
         try:
-            scanner_instance = Scanner()
+            scanner_instance = Scanner()  # Define esta clase según tus necesidades
             sale_id = self.sale()
-            product_list = scanner_instance.recorder()
+            product_list = scanner_instance.recorder()  # Define este método según tus necesidades
             for product in product_list:
                 verify_product = self.search_products(product)
                 if verify_product[0]:
@@ -198,49 +171,38 @@ class data_base():
                         "product_price": verify_product[1][2],
                         "product_code": verify_product[1][3]
                     }
-                    #Default value
+                    # Default value
                     quantity = 1
                         
-                    query = ("INSERT INTO sale_items (sale_id, product_id, quantity, product_price_at_sale) VALUES (%s, %s, %s, %s)")
-                    
+                    query = "INSERT INTO sale_items (sale_id, product_id, quantity, product_price_at_sale) VALUES (%s, %s, %s, %s)"
                     values = (sale_id, product_data["product_id"], quantity, product_data["product_price"])
                     self.cursor.execute(query, values)
                     self.connect.commit()
                     self.delete_products(code=product_data["product_code"], quantity=quantity)
-                    
-        except mysql.connector.Error as err:
-            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-                print("Something is wrong with your user name or password")
-            elif err.errno == errorcode.ER_BAD_DB_ERROR:
-                print("Database does not exist")
-            else:
-                print(err)
-
+        except psycopg2.Error as err:
+            print(f"Error in sale_products: {err}")
+            print("Exception TYPE:", type(err))
+            
     def fake_product_insert(self, fake_cycles: int):
         try:
+            fake = faker.Faker()
             for _ in range(fake_cycles):
                 self.insert_products(
-                    name=faker.word(),
-                    price=faker.random_number(digits=2),
-                    code= faker.ean13(),
-                    quantity=faker.random_number(digits=2),
-                    category=faker.word(),
-                    description=faker.sentence(
-                        nb_words=3, variable_nb_words= False
-                    )
+                    name=fake.word(),
+                    price=fake.random_number(digits=2),
+                    code=fake.ean13(),
+                    quantity=fake.random_number(digits=2),
+                    category=fake.word(),
+                    description=fake.sentence(nb_words=3, variable_nb_words=False)
                 )
-        except mysql.connector.Error as err:
-            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-                print("Something is wrong with your user name or password")
-            elif err.errno == errorcode.ER_BAD_DB_ERROR:
-                print("Database does not exist")
-            else:
-                print(err)
-
+        except psycopg2.Error as err:
+            print(f"Error in fake_product_insert: {err}")
+            print("Exception TYPE:", type(err))
+            
 if __name__ == "__main__":
     db = data_base()
     try:
-        # db.fake_product_insert(10)
+        db.fake_product_insert(10)
         # db.drop_product(
         #     code = 42023
         # )
@@ -256,7 +218,7 @@ if __name__ == "__main__":
         #     code = 27947
         # )
         # print(valor_bool[0])
-        db.sale_products()
+        # db.sale_products()
         pass
     finally:
         db.close()
